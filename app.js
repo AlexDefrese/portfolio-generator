14fd6387e6dc2ea8ab5d3e1f17aeda4adb6d3eff
Alex Defrese <aidandefrese@gmail.com>
2022-09-17 15:50:33 -0400

    dynamically created HTML

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
 
 console.log(profileDataArgs);
 
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
 
 
@@ -25,17 +39,15 @@ console.log(profileDataArgs);
 
 
 
+// const printProfileData = profileDataArr => {
+//     // this
+//     for (let i=0; i < profileDataArr.length; i++){
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('=============');
 
+//     // is the same as
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-const printProfileData = profileDataArr => {
-    // this
-    for (let i=0; i < profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }
-    console.log('=============');
-
-    // is the same as
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
