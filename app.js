66ee018eba9ae3c3fb30827db919974ccfec4441
Alex Defrese <aidandefrese@gmail.com>
2022-09-17 14:33:15 -0400

    started node app learned new feature

---
+++ app.js
@@ -0,0 +1,41 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+console.log(profileDataArgs);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const printProfileData = profileDataArr => {
+    // this
+    for (let i=0; i < profileDataArr.length; i++){
+        console.log(profileDataArr[i]);
+    }
+    console.log('=============');
+
+    // is the same as
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
